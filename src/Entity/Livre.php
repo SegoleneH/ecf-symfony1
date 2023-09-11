7aeceb3245103b8d7f4ea52839d49ef9b857b290
Sego <s.henniart.webdev@gmail.com>
2023-09-11 14:51:06 +0200

    add entities

---
+++ src/Entity/Livre.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivreRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LivreRepository::class)]
+class Livre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 190)]
+    private ?string $titre = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $annee_edition = null;
+
+    #[ORM\Column]
+    private ?int $nombre_pages = null;
+
+    #[ORM\Column(length: 190, nullable: true)]
+    private ?string $code_isbn = null;
+
+    #[ORM\OneToMany(mappedBy: 'livre', targetEntity: Emprunt::class)]
+    private Collection $emprunts;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Auteur $auteur = null;
+
+    #[ORM\ManyToMany(targetEntity: Genre::class)]
+    private Collection $genre;
+
+    public function __construct()
+    {
+        $this->emprunts = new ArrayCollection();
+        $this->genre = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getAnneeEdition(): ?int
+    {
+        return $this->annee_edition;
+    }
+
+    public function setAnneeEdition(?int $annee_edition): static
+    {
+        $this->annee_edition = $annee_edition;
+
+        return $this;
+    }
+
+    public function getNombrePages(): ?int
+    {
+        return $this->nombre_pages;
+    }
+
+    public function setNombrePages(int $nombre_pages): static
+    {
+        $this->nombre_pages = $nombre_pages;
+
+        return $this;
+    }
+
+    public function getCodeIsbn(): ?string
+    {
+        return $this->code_isbn;
+    }
+
+    public function setCodeIsbn(?string $code_isbn): static
+    {
+        $this->code_isbn = $code_isbn;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Emprunt>
+     */
+    public function getEmprunts(): Collection
+    {
+        return $this->emprunts;
+    }
+
+    public function addEmprunt(Emprunt $emprunt): static
+    {
+        if (!$this->emprunts->contains($emprunt)) {
+            $this->emprunts->add($emprunt);
+            $emprunt->setLivre($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEmprunt(Emprunt $emprunt): static
+    {
+        if ($this->emprunts->removeElement($emprunt)) {
+            // set the owning side to null (unless already changed)
+            if ($emprunt->getLivre() === $this) {
+                $emprunt->setLivre(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getAuteur(): ?Auteur
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(?Auteur $auteur): static
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Genre>
+     */
+    public function getGenre(): Collection
+    {
+        return $this->genre;
+    }
+
+    public function addGenre(Genre $genre): static
+    {
+        if (!$this->genre->contains($genre)) {
+            $this->genre->add($genre);
+        }
+
+        return $this;
+    }
+
+    public function removeGenre(Genre $genre): static
+    {
+        $this->genre->removeElement($genre);
+
+        return $this;
+    }
+}

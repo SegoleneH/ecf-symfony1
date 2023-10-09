8c4a4669d143317c252bcc227d0da6d6bd0441c3
Sego <s.henniart.webdev@gmail.com>
2023-10-09 16:37:36 +0200

    réparations->connexion et restrictions OK, fonctions edit pas OK

--- src/Repository/EmpruntRepository.php
+++ src/Repository/EmpruntRepository.php
@@ -14,12 +14,105 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Emprunt[]    findAll()
  * @method Emprunt[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
+
 class EmpruntRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Emprunt::class);
     }
+
+    /**
+     * this method returns the last X emprunt
+     * @param Value $value The number of emprunts to search for
+     * @return Emprunt[] Returns an array of Emprunt objects
+     */
+    public function findLastEmprunt($value): array
+    {
+        return $this->createQueryBuilder('e')
+            ->orderBy('e.dateEmprunt', 'DESC')
+            ->setMaxResults($value)
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * this method returns all emprunts done by a specific emprunteurID
+     * @param Value $value The value of emprunteurId to search for
+     * @return Emprunt[] Returns an array of Emprunt objects
+     */
+    public function findEmpruntByEmprunteurId($value): array
+    {
+        return $this->createQueryBuilder('e')
+            ->select('e')
+            ->where('e.emprunteur = :value')
+            ->setParameter('value', $value)
+            ->orderBy('e.dateEmprunt', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * this method return all emprunt for a specific livreID
+     * @param Value $value The value of LivreId to search for
+     * @return Emprunt[] Returns an array of Emprunt objects
+     */
+    public function findEmpruntByLivreId($value): array
+    {
+        return $this->createQueryBuilder('e')
+            ->select('e')
+            ->where('e.livre = :value')
+            ->setParameter('value', $value)
+            ->orderBy('e.dateEmprunt', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * this method returns the last X emprunt with a return Date
+     * @param Value $value The number of emprunts to search for
+     * @return Emprunt[] Returns an array of Emprunt objects
+     */
+    public function findLastEmpruntRetour($value): array
+    {
+        return $this->createQueryBuilder('e')
+            ->select('e')
+            ->where('e.dateRetour IS NOT null')
+            ->orderBy('e.dateRetour', 'DESC')
+            ->setMaxResults($value)
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * This method finds all Emprunt with no return date
+     * @return Emprunteur[] Returns an array of Emprunt objects
+     */
+    public function findAllNonReturnEmprunt(): array
+    {
+        return $this->createQueryBuilder('e')
+            ->select('e')
+            ->Where('e.dateRetour IS null')
+            ->orderBy('e.dateEmprunt', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * this method returns an emprunt search by a book ID
+     * @param Value $value The value of LivreId to search for
+     * @return Emprunt[] Returns an array of Emprunt objects
+     */
+    public function findEmpruntDataByLivreId($value): array
+    {
+        return $this->createQueryBuilder('e')
+            ->select('e')
+            ->where('e.livre = :value')
+            ->setParameter('value', $value)
+            ->getQuery()
+            ->getResult();
+    }
+}
 
 //    /**
 //     * @return Emprunt[] Returns an array of Emprunt objects
@@ -45,4 +138,4 @@ class EmpruntRepository extends ServiceEntityRepository
 //            ->getOneOrNullResult()
 //        ;
 //    }
-}
+
